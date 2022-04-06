a8aa256d39e8f1f8522805de87699e97ddd1c9b3
MARLIN K MAKORI <marlin.makori@gmail.com>
2022-04-06 12:51:56 -0700

    beta

--- controllers/users.js
+++ controllers/users.js
@@ -12,20 +12,19 @@ module.exports = {
 function index(req, res) {
         User.find({}, function (err, users) {
             let noNewFriends = []
-            if (req.user.friends.length === 0) {
-                noNewFriends.push(users)
-                let userDisplay = noNewFriends.filter(user => user.name !== req.user.name)
-                res.render('friends/search', { userDisplay, notice: req.user.requests.length, requests: req.user.requests, friends: req.user })
-            } 
             for (let i = 0; i < users.length; i++) {
+                if (req.user.friends.length === 0) {
+                    noNewFriends.push(users[i])
+                } else {
                     for (let j = 0; j < req.user.friends.length; j++) {
-                        if (users[i]._id !== req.user.friends[j]._id) {
+                        if (users[i].name !== req.user.friends[j].name) {
                             noNewFriends.push({ _id: users[i]._id, name: users[i].name, avatar: users[i].avatar })
                         }
                     }
+                }
             }
             console.log(noNewFriends)
-            let userDisplay = noNewFriends.filter(user => user._id !== req.user._id)
+            let userDisplay = noNewFriends.filter(user => user.name !== req.user.name)
             res.render('friends/search', { userDisplay, notice: req.user.requests.length, requests: req.user.requests, friends: req.user })
         })
 }
@@ -44,7 +43,7 @@ function sendRequest(req, res) {
 function acceptRequest(req, res) {
     User.findById(req.params.id, (err, requestor) => {
         requestor.friends.push({ _id: req.user._id, name: req.user.name, avatar: req.user.avatar })
-        let friend = req.user.friends.push({ _id: requestor._id, name: requestor.name, avatar: requestor.avatar })
+        let friend = req.user.friends.push({ _id: req.user._id, name: requestor.name, avatar: requestor.avatar })
         let requestIdx = req.user.requests.indexOf(req.params.id)
         let accept = req.user.requests.splice(requestIdx, 1)
         req.user.save()
